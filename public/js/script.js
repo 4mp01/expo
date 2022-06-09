635aca73dd08afb5265f156d637b83b674f37f80
unknown <ijimerusora69@gmail.com>
2022-06-09 16:46:37 +0800

    first commit

---
+++ public/js/script.js
@@ -0,0 +1,50 @@
+
+const socket = io('http://localhost:1999')
+const messageContainer = document.getElementById('message-container')
+const roomContainer = document.getElementById('room-container')
+const messageForm = document.getElementById('send-container')
+const messageInput = document.getElementById('message-input')
+
+if (messageForm != null) {
+  const name = prompt('What is your name?')
+  appendMessage('You joined')
+  socket.emit('new-user', name)
+
+  messageForm.addEventListener('submit', e => {
+    if (messageInput.value == '') {
+      alert('Input something')
+    } else {
+      const message = messageInput.value
+      appendMessage(`You: ${message}`)
+      socket.emit('send-chat-message', message)
+      messageInput.value = ''
+    }
+    e.preventDefault()
+  })
+}
+
+socket.on('room-created', room => {
+  const roomElement = document.createElement('div')
+  roomElement.innerText=room
+  const roomLink = document.createElement('a')
+  roomLink.innerText='join'
+  roomContainer.append(roomElement, roomLink)
+})
+
+socket.on('chat-message', data => {
+  appendMessage(`${data.name}: ${data.message}`)
+})
+
+socket.on('user-connected', name => {
+  appendMessage(`${name} connected`)
+})
+
+socket.on('user-disconnected', name => {
+  appendMessage(`${name} disconnected`)
+})
+
+function appendMessage(message) {
+  const messageElement = document.createElement('div')
+  messageElement.innerText = message
+  messageContainer.append(messageElement)
+}
---
+++ server.js
@@ -0,0 +1,53 @@
+
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
+const path = require('path')
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.use('/public', express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+
+const rooms = {}
+
+app.get('/', (req, res) => {
+  res.render('index', { rooms: rooms })
+})
+
+app.post('/room', (req, res) => {
+  if (rooms[req.body.room != null]) { return res.redirect('/') }
+  rooms[req.body.room] = { users: {} }
+  res.redirect(req.body.room)
+  io.emit('room-created', req.body.room)
+})
+
+app.get('/:room', (req, res) => {
+  if (rooms[req.body.room == null]) { return res.redirect('/') }
+
+  res.render('room', {roomName: req.params.room})
+})
+
+server.listen(1999, () => {
+  console.log("RUNNING")
+})
+
+const users = {}
+
+io.on('connection', socket => {
+  socket.on('new-user', name => {
+    users[socket.id] = name
+    socket.broadcast.emit('user-connected', name)
+  })
+  socket.on('send-chat-message', message => {
+    socket.broadcast.emit('chat-message', { message: message, name: users[socket.id] })
+  })
+
+  socket.on('disconnect', () => {
+    socket.broadcast.emit('user-disconnected', users[socket.id])
+    delete users[socket.id]
+  })
+
+})
