55c12e6ad1ee5e2868e46a7d3eced4b4339a6350
unknown <ijimerusora69@gmail.com>
2022-06-15 03:54:23 +0800

    second commit

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,5 @@
 
-const socket = io('https://www.amphoteric.herokuapp.com/')
+const socket = io('https://amphoteric.herokuapp.com/')
 const messageContainer = document.getElementById('message-container')
 const roomContainer = document.getElementById('room-container')
 const messageForm = document.getElementById('send-container')
