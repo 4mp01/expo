3147eadd9b3bf1c1e9f387264eb3c13d59b19ae3
unknown <ijimerusora69@gmail.com>
2022-06-16 04:44:52 +0800

    third commit

--- public/js/export.js
+++ public/js/export.js
@@ -1,4 +1,4 @@
-export const socket = io('https://amphoteric.herokuapp.com')
+export const socket = io('https://amphoteric.herokuapp.com/')
 export const messageContainer = document.getElementById('message-container')
 export const roomContainer = document.getElementById('room-container')
 export const messageForm = document.getElementById('send-container')
